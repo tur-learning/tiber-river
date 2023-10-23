ff094aee5b796c65d5f1957f15cc0bb38396121c
dario-abbondanza <dario.abbondanza@gmail.com>
2023-10-23 22:21:20 +0200

    Added character to the game

--- temple_assignment/opengl_pygame.py
+++ temple_assignment/opengl_pygame.py
@@ -4,8 +4,118 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+#from OpenGL.GLUT import *
 from PIL import Image
 import asyncio
+
+class OBJLoader:
+    def __init__(self, filename):
+        self.vertices = []
+        self.faces = []
+        
+        try:
+            with open(filename, 'r') as f:
+                for line in f:
+                    if line[:2] == "v ":  # vertices
+                        index1 = line.find(" ") + 1
+                        index2 = line.find(" ", index1 + 1)
+                        index3 = line.find(" ", index2 + 1)
+
+                        vertex = (float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]))
+                        vertex = (round(vertex[0], 2), round(vertex[1], 2), round(vertex[2], 2))
+                        self.vertices.append(vertex)
+
+                    elif line[0] == "f":  # faces
+                        string = line.replace("//", "/")
+                        
+                        i = string.find(" ") + 1
+                        face = []
+                        for item in range(string.count(" ")):
+                            if string.find(" ", i) == -1:
+                                face.append(string[i:-1])
+                                break
+                            face.append(string[i:string.find(" ", i)])
+                            i = string.find(" ", i) + 1
+                        self.faces.append(tuple(face))
+                        
+        except IOError:
+            print(".obj file not found.")
+
+    def render_scene(self):
+        if not self.vertices:
+            return
+
+        glBegin(GL_TRIANGLES)
+        for face in self.faces:
+            for f in face:
+                f = f.split('/')
+                glVertex3fv(self.vertices[int(f[0]) - 1])
+        glEnd()
+
+class Character:
+    def __init__(self):
+        self.position = [0, 0, 0]  # la posizione iniziale del personaggio
+
+    def draw(self):
+        """ Disegna il personaggio. Qui useremo un semplice cubo senza GLUT. """
+        d = 0.004
+        vertices = [  # Vertici per un cubo
+            [-d, -d, -d], [d, -d, -d], [d, d, -d], [-d, d, -d],
+            [-d, -d, d], [d, -d, d], [d, d, d], [-d, d, d]
+        ]
+
+        edges = [  # Spigoli che definiscono ogni faccia del cubo
+            (0, 1), (1, 2), (2, 3), (3, 0),
+            (4, 5), (5, 6), (6, 7), (7, 4),
+            (0, 4), (1, 5), (2, 6), (3, 7)
+        ]
+
+        faces = [  # Vertici per ogni faccia del cubo
+            (0, 1, 2, 3), (4, 5, 6, 7), (0, 1, 5, 4),
+            (2, 3, 7, 6), (0, 3, 7, 4), (1, 2, 6, 5)
+        ]
+
+        glPushMatrix()  # Salva la matrice corrente
+        glTranslate(self.position[0], self.position[1], self.position[2])  # Trasla il cubo
+
+        # Disegna le facce del cubo
+        glBegin(GL_QUADS)
+        for face in faces:
+            for vertex in face:
+                glVertex3fv(vertices[vertex])  # Aggiunge i vertici della faccia
+        glEnd()
+
+        # Disegna gli spigoli del cubo (opzionale, per un aspetto più definito)
+        glBegin(GL_LINES)
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+
+        glPopMatrix()  # Ripristina la matrice precedente
+
+    # def __init__(self, obj_file, position=(0, 0, 0)):
+    #     self.obj = OBJLoader(obj_file)
+    #     self.position = position
+
+    # def draw(self):
+    #     glPushMatrix()
+    #     glTranslate(self.position[0], self.position[1], self.position[2])
+    #     self.obj.render_scene()
+    #     glPopMatrix()
+
+    def move(self, direction):
+        """ Aggiorna la posizione del personaggio in base al comando dell'utente. """
+        l = 0.001
+        if direction == 'FORWARD':
+            self.position[1] += l
+        elif direction == 'BACKWARD':
+            self.position[1] -= l
+        elif direction == 'LEFT':
+            self.position[0] -= l
+        elif direction == 'RIGHT':
+            self.position[0] += l
+
 
 class InteractiveView:
     def __init__(self):
@@ -18,7 +128,9 @@ class InteractiveView:
         self.ly = 0.
         self.texture_id = None
         self.init_gl()
-        self.load_texture("printmaps.png")  # Sostituisci con il tuo percorso dell'immagine
+        self.load_texture("printmaps.png")
+        #self.character = Character('character.obj', position=(0, 0, 0))
+        self.character = Character()
 
     def init_gl(self):
         glEnable(GL_TEXTURE_2D)
@@ -53,7 +165,16 @@ class InteractiveView:
         if keys[pygame.K_DOWN]:
             if self.v_angle > math.radians(3):
                 self.v_angle -= math.radians(1)
+        # Controlla i tasti per muovere il personaggio
         if keys[pygame.K_w]:
+            self.character.move('FORWARD')
+        if keys[pygame.K_s]:
+            self.character.move('BACKWARD')
+        if keys[pygame.K_a]:
+            self.character.move('LEFT')
+        if keys[pygame.K_d]:
+            self.character.move('RIGHT')
+        """ if keys[pygame.K_w]:
             self.ly += 0.01
             self.update_origin()
         if keys[pygame.K_s]:
@@ -64,7 +185,7 @@ class InteractiveView:
             self.update_origin()
         if keys[pygame.K_a]:
             self.lx -= 0.01
-            self.update_origin()
+            self.update_origin() """
         if keys[pygame.K_z]:
             self.K += 0.01
         if keys[pygame.K_LCTRL]:
@@ -95,6 +216,7 @@ class InteractiveView:
         glTexCoord2f(1, 1); glVertex3f(1.0, 1.0, 0.0)
         glTexCoord2f(0, 1); glVertex3f(-1.0, 1.0, 0.0)
         glEnd()
+        self.character.draw()
 
     def capture_screenshot(self, filename='screenshot.png'):
         """
