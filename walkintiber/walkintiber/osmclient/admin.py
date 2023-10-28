2e084eed5bb945fa72c64f453da08c82a41fc6a9
andreagalle <andreagalle@hotmail.it>
2023-10-28 18:32:16 +0200

    new admin action: Get OSM data for selected sector

--- walkintiber/walkintiber/osmclient/admin.py
+++ walkintiber/walkintiber/osmclient/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from .models import OSMSector
+
+@admin.register(OSMSector)
+class OSMSectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_time', 'bbox_upper_left_x', 'bbox_upper_left_y', 'bbox_lower_right_x', 'bbox_lower_right_y',)
+    
+    actions = ['get_osm_data']
+
+    def get_osm_data(self, request, queryset):
+        # assuming you're dealing with only one sector at a time
+        sector = queryset.first() 
+        bbox = f"{sector.bbox_upper_left_x},{sector.bbox_upper_left_y},{sector.bbox_lower_right_x},{sector.bbox_lower_right_y}"
+
+        # assuming you have a URL pattern named 'get_osm' that accepts 'bbox' as a query parameter
+        url = reverse('get_osm') + f'?bbox={bbox}' 
+
+        return HttpResponseRedirect(url)
+    get_osm_data.short_description = "Get OSM data for selected sector"
--- walkintiber/walkintiber/osmclient/models.py
+++ walkintiber/walkintiber/osmclient/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class OSMSector(models.Model):
+    name = models.CharField(max_length=255, help_text="A descriptive name for this sector.")
+    date_time = models.DateTimeField(auto_now_add=True, help_text="The date and time this sector was defined.")
+    bbox_upper_left_x = models.FloatField(help_text="Bounding box left side (longitude).")
+    bbox_upper_left_y = models.FloatField(help_text="Bounding box lower side (latitude).")
+    bbox_lower_right_x = models.FloatField(help_text="Bounding box right side (longitude).")
+    bbox_lower_right_y = models.FloatField(help_text="Bounding box upper side (latitude).")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "OSM Sector"
+        verbose_name_plural = "OSM Sectors"
